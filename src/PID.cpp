5d5c228329b68ff2065be6c2014ca405cdfba0d9
russ32768 <russ32768@yahoo.com>
2019-12-20 02:35:29 +0000

    initial commit

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,6 +12,9 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
 
 }
 
@@ -20,11 +23,16 @@ void PID::UpdateError(double cte) {
    * TODO: Update PID errors based on cte.
    */
 
+  d_error = (cte - p_error);
+  p_error = cte;
+  i_error += cte;
+
 }
 
 double PID::TotalError() {
   /**
    * TODO: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+  //return 0.0;  // TODO: Add your total error calc here!
+  return -Kp * p_error - Kd * d_error - Ki * i_error;
 }
